2853661d1b20383b88ba6e13beec503296696581
frontendm7md <meedo1862010@gmail.com>
2024-11-29 08:03:54 +0200

    Create rightData

--- src/components/page.js
+++ src/components/page.js
@@ -50,7 +50,96 @@ function Page() {
                         </div>
                     </div>
                 </div>
-                <div className="right-data"></div>
+                <div className="right-data">
+                    <div className="healthData">
+                        <div className="cards">
+                            <img src="/media/heart-rate.jpg" alt=""></img>
+                            <span>Heart Rate</span>
+                            <div>
+                                <h2>{user.healthInfo.heartRate.value}</h2>
+                                <h5>{user.healthInfo.heartRate.unit}</h5>
+                            </div>
+                        </div>
+                        <div className="cards">
+                            <img src="/media/body-temperature.jpg" alt=""></img>
+                            <span>Body Temperature</span>
+                            <div>
+                                <h2>{user.healthInfo.bodyTemperature.value}</h2>
+                                <h5>{user.healthInfo.bodyTemperature.unit}</h5>
+                            </div>
+                        </div>
+                        <div className="cards">
+                            <img src="/media/glucose.jpg" alt=""></img>
+                            <span>Glucose</span>
+                            <div>
+                                <h2>{user.healthInfo.glucose.value}</h2>
+                                <h5>{user.healthInfo.glucose.unit}</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="testReports">
+                        <h3>Test Reports</h3>
+                        <div className="reports">
+                            <div className="frame">
+                                <ion-icon name="medkit" id="first"></ion-icon>
+                                <div>
+                                    <h4>CT Scan -Full Body</h4>
+                                    <h5>12th February 2020</h5>
+                                </div>
+                            </div>
+                            <div className="frame">
+                                <ion-icon name="medkit" id="second"></ion-icon>
+                                <div>
+                                    <h4>Creatine Kinase T</h4>
+                                    <h5>12th February 2020</h5>
+                                </div>
+                            </div>
+                            <div className="frame">
+                                <ion-icon name="medkit" id="third"></ion-icon>
+                                <div>
+                                    <h4>Eye Fluorescein Test</h4>
+                                    <h5>12th February 2020</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="prescriptions">
+                        <h3>Prescriptions</h3>
+                        <div className="addPrescription">
+                            <ion-icon name="add-outline"></ion-icon>
+                            <h5>Add Prescription</h5>
+                        </div>
+                        <div className="prescriptionsData">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <td>Prescriptions</td>
+                                        <td>Date</td>
+                                        <td>Duration</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td className="name">
+                                            <ion-icon name="document"></ion-icon>
+                                            <h4>{user.PrescriptionsData.first.name}</h4>
+                                        </td>
+                                        <td>{user.PrescriptionsData.first.date}</td>
+                                        <td>{user.PrescriptionsData.first.duration}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="name">
+                                            <ion-icon name="document"></ion-icon>
+                                            <h4>{user.PrescriptionsData.second.name}</h4>
+                                        </td>
+                                        <td>{user.PrescriptionsData.second.date}</td>
+                                        <td>{user.PrescriptionsData.second.duration}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/data/user.js
+++ src/data/user.js
@@ -15,6 +15,17 @@ let user = {
         Weight: "65 kg",
         PatientID: "208898786",
         LastVisit: "25th October 2019"
+    },
+
+    healthInfo: {
+        heartRate: { value: "80", unit: "bpm" },
+        bodyTemperature: { value: "36.5", unit: "°C" },
+        glucose: { value: "100", unit: "mg/dl" }
+    },
+
+    PrescriptionsData: {
+        first: { name: "Heart Diseases", date: "25th October 2019", duration: "3 months" },
+        second: { name: "Skin Care", date: "8th August 2019", duration: "2 months" }
     }
 };
 
