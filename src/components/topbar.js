31298756d15323666718e873085ca4b1249cce10
frontendm7md <meedo1862010@gmail.com>
2024-12-11 19:37:13 +0200

    Create user-menu

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -5,6 +5,7 @@ import { ThemeContext } from "../App";
 function TopBar() {
     let { theme, setTheme } = useContext(ThemeContext);
     const [iconName, setIconName] = useState("moon-outline");
+    const [showUserData, setShowUserData] = useState(false);
     let changeTheme = () => {
         setTheme(theme === "light" ? "dark" : "light");
         setIconName((prevName) => (prevName === "moon-outline" ? "sunny-outline" : "moon-outline"));
@@ -17,11 +18,28 @@ function TopBar() {
             </div>
 			<div className="data">
                 <ion-icon name={iconName} onClick={changeTheme} id="theme"></ion-icon>
-                <div className="userData">
+                <div className="userData" onClick={() => setShowUserData(!showUserData)}>
                     <img src={"/media/user-image.jpg"} alt=""></img>
                     <h5>Dr. Alex Hess</h5>
                     <ion-icon name="chevron-down-outline"></ion-icon>
                 </div>
+                {showUserData && (
+                    <div className={"user-menu" + (theme === "light" ? "" : " dark")}>
+                        <div className="user-info">
+                            <img src={"/media/user-image.jpg"} alt="User" />
+                            <div>
+                                <h4>Dr. Alex Hess</h4>
+                                <span>alex@gmail.com</span>
+                            </div>
+                        </div>
+                        <div className="profile-option">
+                            <span>Create New Profile</span>
+                        </div>
+                        <div className="logout-option">
+                            <span>Log Out</span>
+                        </div>
+                    </div>
+                )}
             </div>
 		</div>
 	);
