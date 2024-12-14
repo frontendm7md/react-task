f351a935e15bbef765e359bcfa282b585d3de5a9
frontendm7md <meedo1862010@gmail.com>
2024-12-15 00:25:27 +0200

    Create AddPrescriptions option

--- src/components/page.js
+++ src/components/page.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { useContext } from "react";
 import { ThemeContext } from "../App";
 import user from "../data/user";
@@ -86,8 +87,63 @@ function Page() {
     }
 
     let { theme } = useContext(ThemeContext);
+    const [AddPrescription, SetAddPrescription] = useState(false);
+    let [NewPrescription, SetNewPrescription] = useState({
+        name: "",
+        date: "",
+        duration: ""
+    })
+    function AddNewPrescription() {
+        SetAddPrescription(!AddPrescription)
+        user.prescriptions.push(NewPrescription)
+        SetNewPrescription({name: "", date: "", duration: ""})
+    }
     return (
         <div className={"page" + (theme === "light" ? "" : " dark")}>
+            {AddPrescription && (
+                <div className="background">
+                    <div className={"card" + (theme === "light" ? "" : " dark")}>
+                        <h2>Add Prescription</h2>
+                        <div className="inputs">
+                            <div className="form-control">
+                                <input type="value" value={NewPrescription.name} onChange={(event) => {SetNewPrescription({name: event.target.value, date: NewPrescription.date, duration: NewPrescription.duration})}} className={"input" + (theme === "light" ? "" : " dark")} required />
+                                <label>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '0ms'}}>N</span>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '50ms'}}>A</span>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '100ms'}}>M</span>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '150ms'}}>E</span>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <input type="value" value={NewPrescription.date} onChange={(event) => {SetNewPrescription({name: NewPrescription.name, date: event.target.value, duration: NewPrescription.duration})}} className={"input" + (theme === "light" ? "" : " dark")} required />
+                                <label>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '0ms'}}>D</span>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '50ms'}}>A</span>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '100ms'}}>T</span>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '150ms'}}>E</span>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <input type="value" value={NewPrescription.duration} onChange={(event) => {SetNewPrescription({name: NewPrescription.name, date: NewPrescription.date, duration: event.target.value})}} className={"input" + (theme === "light" ? "" : " dark")} required />
+                                <label>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '0ms'}}>D</span>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '50ms'}}>U</span>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '100ms'}}>R</span>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '150ms'}}>A</span>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '200ms'}}>T</span>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '250ms'}}>I</span>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '300ms'}}>O</span>
+                                    <span className={"span" + (theme === "light" ? "" : " dark")} style={{transitionDelay: '350ms'}}>N</span>
+                                </label>
+                            </div>
+                        </div>
+                        <div className="btns">
+                            <button onClick={() => SetAddPrescription(!AddPrescription)}>Cancel</button>
+                            <button onClick={AddNewPrescription}>Add</button>
+                        </div>
+                    </div>
+                </div>
+            )}
             <h2>Current Appointment</h2>
             <div className="Page-info">
                 <div className="left-data">
@@ -116,7 +172,7 @@ function Page() {
                     </div>
                     <div className={"prescriptions" + (theme === "light" ? "" : " dark")}>
                         <h3>Prescriptions</h3>
-                        <div className="addPrescription">
+                        <div className="addPrescription" onClick={() => SetAddPrescription(!AddPrescription)}>
                             <ion-icon name="add-outline"></ion-icon>
                             <h5>Add Prescription</h5>
                         </div>
